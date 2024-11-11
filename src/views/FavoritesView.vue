f8198d17d503afd4244c489c4f46d2a4175fb64f
Stanislav Shchelokov <stanislavschelokov@gmail.com>
2024-11-11 21:29:05 +0200

    initial commit

---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="about">favorites</div>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Home <span>test</span></div>
+</template>
+
+<script setup></script>
+<style lang="scss" scoped>
+span {
+    color: $red;
+}
+</style>
